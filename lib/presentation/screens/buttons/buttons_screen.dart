ca65f7766b4b62414ddbc2bf28ad7e1e0eb780ce
Sander Castillo <sandergcc@gmail.com>
2024-02-12 18:19:32 -0500

    route names

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class ButtonsScreen extends StatelessWidget {
+
+
+  static const String name = 'buttons';
+
   const ButtonsScreen({super.key});
 
   @override
